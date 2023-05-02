71fdb10d3c2cada110e16979d393ca01a7838e00
rokasj <rokas@jerio.me>
2023-05-02 18:01:55 -0400

    Add marks

--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -13,7 +13,7 @@ class View {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         let li = document.createElement('li')
-        li.dataset.pos = [i, j] //<li data-pos="i"></li>
+        li.dataset.pos = [i, j] //<li data-pos="i"></li>    ---IF WE WANTED TO DO THIS RIGHT, WE NEEDED TO DO JSON.stringify()
         console.log(typeof li.dataset.pos)
         ul.append(li)
       }
@@ -43,13 +43,18 @@ class View {
     let stringPos = e.target.dataset.pos
     let pos = [parseInt(stringPos[0]), parseInt(stringPos[2])]
     console.log(pos)
-    console.log(this.game.board)
+    // console.log(this.game.board)   ///the console print DOES update live, so you won't see an empty board here
     this.game.playMove(pos)
     console.log(this.game.board)
+    this.makeMove(stringPos, pos)
     // const ul = document.querySelector('.grid')
   }
 
-  makeMove(square) {
+  makeMove(stringPos, pos) {
+    let mark = this.board.grid[pos[0]][pos[1]]
+    let li = document.querySelector(`[data-pos="${stringPos}"]`)
+    console.log(li)
+    li.innerText = mark
   }
   
   handleGameOver() {
